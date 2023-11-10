0a7d708d91b8756e35f8dd416078e4a10253ba20
gintamafan25 <symphonian25@hotmail.com>
2023-11-10 00:21:33 +0000

    index API up

--- Fights/boxing/views.py
+++ Fights/boxing/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from django.core import serializers
+from .models import *
 # Create your views here.
 
 def index(request):
-    list = ["empty"]
-    return JsonResponse({"list": list})
+    try:
+        data = Fighters.objects.all()
+        fighters = serializers.serialize("json", data)
+        return JsonResponse({"fighters":fighters})
+    except TypeError:
+        return JsonResponse({"empty":"empty"})
     
