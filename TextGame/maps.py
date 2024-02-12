30f2854d9eff87efe12cc26bd431b3fec0665a9e
Gintamafan25 <130246102+Gintamafan25@users.noreply.github.com>
2024-02-12 20:27:07 +0000

    added tiles

--- TextGame/maps.py
+++ TextGame/maps.py
@@ -1,9 +1,29 @@
 
 class Map:
-    def __init__(self, height, width):
+    def __init__(self,name, height, width):
+        self.name = name
         self.height = height
         self.width = width
         self.tiles = {}
+    
+    def create_map(self):
+        for i in self.height:
+            for j in self.width:
+                self.tiles[(i,j)] = []
+
+class Tiles:
+    Objects = ["Rock", "Swamp"]
+
+    def __init__(self, tile):
+        self.tile = tile
+    
+    def check(self):
+        for obj in self.Objects:
+            if obj in self.tile:
+                return False
+    
+        
+
     
 
        
