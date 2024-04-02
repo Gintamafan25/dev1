e195b40bb6357ebc1a3e1afe9751fb39ecc0748d
Gintamafan25 <130246102+Gintamafan25@users.noreply.github.com>
2024-04-02 13:36:31 -0400

    Delete Page directory

--- Page/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { FormComponent } from './form/form.component'
-import { AppComponent } from './app.component'
-
-export const routes: Routes = [
-    {path: "", title: "Home Page", component:AppComponent},
-    {path: "register", title:"register", component:FormComponent}
-
-];
